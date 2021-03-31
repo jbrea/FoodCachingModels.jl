b23d713b57a22aca0be3be4167b6eab11a1969a1
Johanni Brea <jbrea@users.noreply.github.com>
2021-03-31 18:21:54 +0200

    refactor

--- src/trackers.jl
+++ src/trackers.jl
@@ -1,14 +1,19 @@
 struct Tracker
 	fieldnames::Array{Any, 1}
-	data::DataFrame
+	data::Dict
+    last::Dict
 end
 struct NoTracker end
+struct TimeSeries{T,V}
+    t::T
+    v::V
+end
 
 function Tracker(; trackedfields = [], kargs...)
 	if length(trackedfields) == 0
 		NoTracker()
 	else
-		Tracker(trackedfields, DataFrame(t = Quantity[], field = [], value = []))
+        Tracker(trackedfields, Dict(), Dict())
 	end
 end
 
@@ -23,42 +28,54 @@ function getdeepfield(o, f::Tuple)
 end
 getdeepfield(o, f::Function) = f(o)
 
+function dpush!(dict, key, t, v)
+    if haskey(dict, key)
+        push!(dict[key].t, t)
+        push!(dict[key].v, v)
+    else
+        dict[key] = TimeSeries([t], [v])
+    end
+end
+
 track!(::NoTracker, w, t) = nothing
 function track!(tracker::Tracker, m, t)
 	for field in tracker.fieldnames
-        field ∉ (:weights, :actions) && push!(tracker.data, [t, field, getdeepfield(m, field)])
-	end
-end
-
-trackweights!(::NoTracker, w, t) = nothing
-function trackweights!(tracker::Tracker, w, t)
-	if :weights in tracker.fieldnames
-        push!(tracker.data, [t, :weights, deepcopy(w)])
+        if field ∉ (:actions, )
+            v = getdeepfield(m, field)
+            k = last(field)
+            if haskey(tracker.last, k)
+                if !haskey(tracker.data, k) ||
+                   !(v == tracker.last[k].v == last(tracker.data[k].v))
+                    dpush!(tracker.data, k, tracker.last[k].t, tracker.last[k].v)
+                end
+            end
+            tracker.last[k] = (t = t, v = v)
+        end
 	end
 end
 
 trackeat!(::NoTracker, w, o, t) = nothing
 function trackeat!(tracker::Tracker, w, o, t)
     if :actions in tracker.fieldnames
-        push!(tracker.data, [t, :actions, ("eat", w, o.id)])
+        dpush!(tracker.data, :actions_eat, t, (w, o.id))
     end
 end
 trackinspect!(::NoTracker, w, o, t) = nothing
 function trackinspect!(tracker::Tracker, w, tray, t)
     if :actions in tracker.fieldnames
-        push!(tracker.data, [t, :actions, ("inspect", w, tray.appearance)])
+        dpush!(tracker.data, :actions_inspect, t, (w, tray.appearance))
     end
 end
 trackcache!(::NoTracker, w, o, tray, t) = nothing
 function trackcache!(tracker::Tracker, w, o, tray, t)
     if :actions in tracker.fieldnames
-        push!(tracker.data, [t, :actions, ("cache", w, o.id, tray.appearance)])
+        dpush!(tracker.data, :actions_cache, t, (w, o.id, tray.appearance))
     end
 end
 trackotheraction!(::NoTracker, w, t) = nothing
 function trackotheraction!(tracker::Tracker, w, t)
     if :actions in tracker.fieldnames
-        push!(tracker.data, [t, :actions, ("other", w)])
+        dpush!(tracker.data, :actions_other, t, (w,))
     end
 end
 
